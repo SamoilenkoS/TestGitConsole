799e69a64c88300dfc442a6b0c840e9268ed34cc
Sviatoslav Samoilenko <sviatoslavs@wizardsdev.com>
2021-07-15 21:31:11 +0300

    Change A

--- ConsoleApp3/Arrow.cs
+++ ConsoleApp3/Arrow.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ConsoleApp3
 {
     [Serializable]
     public class Arrow : AbstractFigure
     {
+        //
     }
 }
